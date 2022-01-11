622d6491b875dd25a3383b3f22c95eddecb9a5e8
Sumeet <sumeetoc@gmail.com>
2022-01-11 15:14:42 +0530

    gcs

---
+++ terraform/dev/main.tf
@@ -0,0 +1,8 @@
+resource "google_storage_bucket" "org_terraform_state" {
+  project = "prj-phi-ss-logging-gfs4"
+
+  name     = "prj-phi-ss-logging-gfs4-test"
+  location = "us-central1"
+
+  force_destroy               = true
+}
